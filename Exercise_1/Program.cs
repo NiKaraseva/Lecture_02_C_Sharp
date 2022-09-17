33cba932831cfa7bb7c21289c5ab42feba57ea75
Nika <nikarasevas@gmail.com>
2022-09-17 21:04:57 +0300

    Запись 4-ёх строк в 1 – нахождение max из большого кол-ва чисел с помощью функции

--- Exercise_1/Program.cs
+++ Exercise_1/Program.cs
@@ -10,7 +10,7 @@ int a1 = 15;
 int b1 = 211;
 int c1 = 39;
 
-int a2 = 12;
+int a2 = 12555;
 int b2 = 23;
 int c2 = 33;
 
@@ -18,9 +18,11 @@ int a3 = 13;
 int b3 = 2411;
 int c3 = 31;
 
-int max1 = Max(a1, b1, c1);
+/*int max1 = Max(a1, b1, c1);
 int max2 = Max(a2, b2, c2);
 int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+int max = Max(max1, max2, max3);*/
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3)); // Верхние 4 строчки можно написать одной строкой
 
 Console.WriteLine(max);
