b92e5de0abc0afdf236fc77f9f4c324f670cd641
Nika <nikarasevas@gmail.com>
2022-09-18 21:05:57 +0300

    Заполнили массив рандомными эл-тами + нашли индекс элемента find

--- Exercise_Array3/Program.cs
+++ Exercise_Array3/Program.cs
@@ -1,4 +1,4 @@
-﻿void FillArray(int[] collection) // Назначаем элементы в массив
+﻿void FillArray(int[] collection) // назначаем элементы в массив
 // void метод – метод, который ничего не возвращает
 // collection – это просто название аргумента
 {
@@ -22,7 +22,35 @@ void PrintArray(int[] col) // делаем метод, который напеч
     }
 }
 
-int[] array = new int[10]; // По умолчанию все 0-и, выше задаём рандомные значения элементам массива (FillArray) и выводим их (PrintArray)
 
-FillArray(array); //Назначает элементы в массив
-PrintArray(array); //Распечатывает массив
+int IndexOf(int[] collection, int find) // ищем индекс find в рандомном массиве
+// IndexOf - название функции, int[]collection - массив, int find - элемент массива
+{
+    int count = collection.Length; 
+    int index = 0; 
+    int position = -1; /* новая переменная, куда мы положим значение искомого индекса,
+    ставим -1, чтобы, если эл-та с таким значением нет в массиве – отобразилось -1*/
+    
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index; // кладём инндекс в новую пересенную 
+            break;
+        }
+        index++;
+    }
+    return position; // возвращаем значение
+}
+
+int[] array = new int[10]; // обозначили массив array. По умолчанию все 0-и, выше задаём рандомные значения элементам массива (FillArray) и выводим их (PrintArray)
+
+FillArray(array); //назначает элементы в массив
+array[7] = 4; // сами можем добавить определённое значение на определённое место
+array[8] = 4;
+array[9] = 4;
+PrintArray(array); //распечатывает массив
+Console.WriteLine(); //  выводим пустую строчку
+
+int pos = IndexOf(array, 4); // новая переменная равна значению функции + ищем 4ый элемент
+Console.WriteLine(pos);
